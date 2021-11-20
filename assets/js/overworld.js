c49d554d11421ac72f93983b12661f46baa9b288
Kledenai Ashver <bruno.silva1643@etec.sp.gov.br>
2021-11-20 11:48:30 -0300

    finish part1 of the project

---
+++ assets/js/overworld.js
@@ -0,0 +1,50 @@
+class Overworld {
+  constructor(config) {
+    this.element = config.element;
+    this.canvas = this.element.querySelector(".game-canvas");
+    this.ctx = this.canvas.getContext("2d");
+  }
+
+  init() {
+    const image = new Image();
+    image.onload = () => {
+      this.ctx.drawImage(image,0,0)
+    };
+    image.src = "/assets/img/maps/DemoLower.png";
+
+    const x = 5;
+    const y = 6;
+
+    const shadow = new Image();
+    shadow.onload = () => {
+      this.ctx.drawImage(
+        shadow,
+        0,
+        0,
+        32,
+        32,
+        x * 16 - 8,
+        y * 16 - 18,
+        32,
+        32
+    )
+    }
+    shadow.src = "/assets/img/characters/shadow.png";
+
+    const hero = new Image();
+    hero.onload = () => {
+      this.ctx.drawImage(
+        hero,
+        0,
+        0,
+        32,
+        32,
+        x * 16 - 8,
+        y * 16 - 18,
+        32,
+        32
+      )
+    }
+    hero.src = "/assets/img/characters/people/hero.png";
+  }
+}
